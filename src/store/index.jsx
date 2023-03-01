d541a443070da3bbebdf7f5ac5308f5fef9626af
sugiartofahmi <sugiartofahmi@gmail.com>
2023-03-02 06:53:03 +0700

    Fahmi Sugiartoo | Restructure Project

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -5,14 +5,14 @@ export const locationState = atom({
   default: "bandung",
 });
 
-export const fetchWeather = atom({
+export const fetchWeather = selector({
   key: "fetch-weather",
   get: async ({ get }) => {
     try {
       const res = await axios.get(
         import.meta.env.VITE_WEATHER_URL + get(locationState)
       );
-      return res.data || [];
+      return res.data || {};
     } catch (error) {
       console.log(error);
     }
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,4 +1,12 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { fetchWeather, locationState } from "../../store";
+import axios from "axios";
+import { useEffect } from "react";
 const Home = () => {
+  const weather = useRecoilValue(fetchWeather);
+  const [location, setLocation] = useRecoilState(locationState);
+
+  console.log(weather);
   return <h1>Home</h1>;
 };
 
