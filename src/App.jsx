13a4ddbb06fa7e304e0a6605bc5a44319a80824f
sugiartofahmi <sugiartofahmi@gmail.com>
2023-03-02 11:16:26 +0700

    Fahmi Sugiarto | Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ const Router = lazy(() => import("./router"));
 const MainLayout = lazy(() => import("./layouts/MainLayout"));
 const ContentLayout = lazy(() => import("./layouts/ContentLayout"));
 const Loading = lazy(() => import("./components/Loading"));
+const Error = lazy(() => import("./components/Error"));
+import { ErrorBoundary } from "react-error-boundary";
 const App = () => {
   return (
     <Suspense fallback={<Loading />}>
       <MainLayout>
-        <Suspense fallback={<Loading />}>
-          <ContentLayout>
-            <Router />
-          </ContentLayout>
-        </Suspense>
+        <ErrorBoundary fallback={<Error />}>
+          <Suspense fallback={<Loading />}>
+            <ContentLayout>
+              <Router />
+            </ContentLayout>
+          </Suspense>
+        </ErrorBoundary>
       </MainLayout>
     </Suspense>
   );
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,5 +1,6 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import { fetchWeather, locationState } from "../../store";
+import { ErrorBoundary } from "react-error-boundary";
 import Card from "../../components/Card";
 const Home = () => {
   const weather = useRecoilValue(fetchWeather);
@@ -27,39 +28,43 @@ const Home = () => {
           placeholder="Search location"
         />
       </div>
-      <div className="w-full flex flex-row justify-between items-center text-xl px-[7vh]">
-        <div>
-          <h2 className="text-base">
-            {weather.name} - {weather.sys.country}
-          </h2>
-          <h2 className="text-5xl font-bold">
-            {Math.round((weather.main.temp - 32) * 0.555)}
-            <span>&#8451;</span>
-          </h2>
-        </div>
-        <h2>{weather.weather[0].main}</h2>
-      </div>
-      <div className="w-full flex flex-col justify-center items-center text-xl px-[7vh]">
-        <Card>
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="font-semibold">
-              {Math.round((weather.main.feels_like - 32) * 0.555)}
+      <ErrorBoundary
+        fallback={<h1 className="text-red-500">Could not fetch data.</h1>}
+      >
+        <div className="w-full flex flex-row justify-between items-center text-xl px-[7vh]">
+          <div>
+            <h2 className="text-base">
+              {weather.name} - {weather.sys.country}
+            </h2>
+            <h2 className="text-5xl font-bold">
+              {Math.round((weather.main.temp - 32) * 0.555)}
               <span>&#8451;</span>
-            </h1>
-            <h1>Feels Like</h1>
+            </h2>
           </div>
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="font-semibold">{weather.main.humidity}%</h1>
-            <h1>Humidity</h1>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="font-semibold">
-              {Math.round(weather.wind.speed)}MPH
-            </h1>
-            <h1>Winds</h1>
-          </div>
-        </Card>
-      </div>
+          <h2>{weather.weather[0].main}</h2>
+        </div>
+        <div className="w-full flex flex-col justify-center items-center text-xl px-[7vh]">
+          <Card>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="font-semibold">
+                {Math.round((weather.main.feels_like - 32) * 0.555)}
+                <span>&#8451;</span>
+              </h1>
+              <h1>Feels Like</h1>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="font-semibold">{weather.main.humidity}%</h1>
+              <h1>Humidity</h1>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="font-semibold">
+                {Math.round(weather.wind.speed)}MPH
+              </h1>
+              <h1>Winds</h1>
+            </div>
+          </Card>
+        </div>
+      </ErrorBoundary>
     </div>
   );
 };
